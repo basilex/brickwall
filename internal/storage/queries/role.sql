5de930d5a847808a8c927d4e4a50ba51b556736c
Alexander Vasilenko <alexander.vasilenko@gmail.com>
2025-03-19 11:46:27 +0200

    Role: base db queries added

---
+++ internal/storage/queries/role.sql
@@ -0,0 +1,21 @@
+-- name: RoleNew :one
+insert into role(name) values(@name) returning *;
+
+-- name: RoleSelect :many
+select *
+  from role r
+ order by @sql_order::text
+ limit @sql_limit offset @sql_offset;
+
+-- name: RoleSelectByID :one
+select * from role r where r.id = @id;
+
+-- name: RoleSelectByName :one
+select * from role r where r.name = @name;
+
+-- name: RoleUpdateByID :one
+update role
+   set name = @name where id = @id returning *;
+
+-- name: RoleDeleteByID :one
+delete from role where id = @id returning id;
